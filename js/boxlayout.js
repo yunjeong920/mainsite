c174a52a25d9e4f3139a6152f9e11b44254eccf4
yunjeong <yunjeong920@naver.com>
2021-06-07 15:14:03 +0900

    수정

--- js/boxlayout.js
+++ js/boxlayout.js
@@ -138,3 +138,13 @@ window.onclick = function(){
     skill.find(".inner-bar").animate({width: percentage}, 1200);
   });
 }
+
+$(function(){
+	$("ul.tabs li").click(function(){
+		var tab_id = $(this).attr("data-tab");
+		$("ul.tabs li").removeClass("current");
+		$(".tab-content").removeClass("current");
+		$(this).addClass("current");
+		$("#"+tab_id).addClass("current");
+	})
+})
