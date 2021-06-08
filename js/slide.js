c9d07a3fc9cdcb9931357905d3cf1ab1e8e2cb4a
yunjeong <yunjeong920@naver.com>
2021-06-08 23:01:40 +0900

    초기화 수정

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+$(function() {
+  let oneswiper = new Swiper(".firstSwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
